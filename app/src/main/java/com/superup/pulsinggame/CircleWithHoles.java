a699105adbff7577e3f8cc05eb83632d0a6ea2b6
Eido A <eido@elsight.com>
2018-12-07 18:35:06 +0200

    Add 1 circle draw on the center of the screen

--- app/src/main/java/com/superup/pulsinggame/CircleWithHoles.java
+++ app/src/main/java/com/superup/pulsinggame/CircleWithHoles.java
@@ -1,29 +1,72 @@
 package com.superup.pulsinggame;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.support.annotation.Nullable;
 
-import com.superup.engine.actors.SimpleMovingActor;
+import com.superup.engine.actors.PositionedActor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Hoop shaped circle, which has holes in its stroke.
+ * Hoop shaped circle, which has 1-3 holes in its stroke.
  */
-public class CircleWithHoles extends SimpleMovingActor {
+public class CircleWithHoles extends PositionedActor {
+    public static int RADIUS = 10;
+    public static byte MAX_HOLES = 3;
+    public int maxRadius = 1;
 
+    private final List<CircleHole> holes;
+
+    /**
+     * @param x initial circle X position
+     * @param y initial circle Y position
+     */
     public CircleWithHoles(int x, int y) {
-        super(x, y);
+        super(x, y, RADIUS, RADIUS);
+
+        this.holes = new ArrayList<>();
+
+        this.generateHoles();
     }
 
-    //region SimpleMovingActor Implementations
+    private void generateHoles() {
+
+    }
+
+    public void expand() {
+        if (this.isEnabled()) {
+
+        }
+    }
+
     @Override
     public void stylePaint(Paint p) {
-
+        p.setColor(Color.WHITE);
+        p.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     public void draw(Canvas canvas) {
+        canvas.drawRoundRect(this.getRectF(), this.getWidth(), this.getHeight(), this.getPaint());
+    }
+
+    @Override
+    public void setAlpha(int i) {
 
     }
-    //endregion
 
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.OPAQUE;
+    }
 }
--- app/src/main/java/com/superup/pulsinggame/MainPanel.java
+++ app/src/main/java/com/superup/pulsinggame/MainPanel.java
@@ -3,37 +3,68 @@ package com.superup.pulsinggame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.superup.engine.AbstractGamePanel;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 import java.util.List;
 
 /**
  * Main game panel.
  */
 class MainPanel extends AbstractGamePanel {
+    private List<CircleWithHoles> circles = new ArrayList<>();
+
+    private boolean paused;
 
     public MainPanel(Context context) {
         super(context);
+
+        super.setPanelColor(Color.BLACK);
     }
 
+    //region AbstractGamePanel
     @Override
     public void onStart() {
-
+        this.initializeCircles();
     }
 
+    private void initializeCircles() {
+        this.addCircle();
+    }
+
+    private void addCircle() {
+        int centerX = this.getWidth() / 2;
+        int centerY = this.getHeight() / 2;
+
+        CircleWithHoles circleWithHoles = new CircleWithHoles(centerX, centerY);
+
+        this.circles.add(circleWithHoles);
+    }
+
+    private void expandCircles() {
+        for (CircleWithHoles circle : this.circles) {
+            circle.expand();
+        }
+    }
+
+    //region AbstractGamePanel Implementations
     @Override
     public void onTimer() {
-
+        if (this.paused == false) {
+            this.circles.get(0).expand();
+        }
     }
 
     @Override
     public void redrawCanvas(Canvas canvas) {
-
+        this.circles.get(0).draw(canvas);
     }
 
     @Override
@@ -43,6 +74,9 @@ class MainPanel extends AbstractGamePanel {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        Log.d("MainPanel", "onTouchEvent: touched!");
+        
         return false;
     }
+    //endregion
 }
