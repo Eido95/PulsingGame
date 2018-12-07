5190825e26a4446314d82e48cb000984a76a5a24
Eido A <eido@elsight.com>
2018-12-07 17:10:24 +0200

    Fix app screen mode to be full screen

--- app/src/main/java/com/superup/pulsinggame/MainActivity.java
+++ app/src/main/java/com/superup/pulsinggame/MainActivity.java
@@ -1,13 +1,16 @@
 package com.superup.pulsinggame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.superup.engine.GameActivity;
 
+public class MainActivity extends GameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        this.switchImmersive();
+        this.setContentView(new MainPanel(this));
     }
 }
--- engine/src/main/java/com/superup/engine/GameActivity.java
+++ engine/src/main/java/com/superup/engine/GameActivity.java
@@ -2,6 +2,7 @@ package com.superup.engine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -19,5 +20,18 @@ public class GameActivity extends AppCompatActivity {
 		// making it full screen
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 		   WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
+
+	/**
+	 * Switch the screen mode to be "immersive".
+	 * @See <a href="https://developer.android.com/training/system-ui/immersive>
+	 *       Enable fullscreen mode</a>
+	 */
+	protected void switchImmersive() {
+		this.getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+		);
 	}
 }
